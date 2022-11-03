bc01d4b840f52710def532927f9909db935525f3
Emmanuel Obeng <emmallob14@gmail.com>
2022-11-03 21:28:16 +0000

    Insert all categories and nominees

--- app/Database/Migrations/query.sql
+++ app/Database/Migrations/query.sql
@@ -1,4 +1,71 @@
-ALTER TABLE `surveys`  ADD `created_by` VARCHAR(32) NULL DEFAULT NULL  AFTER `users_logs`;
-ALTER TABLE `surveys_questions`  ADD `created_by` VARCHAR(32) NULL DEFAULT NULL  AFTER `status`;
-ALTER TABLE `surveys` CHANGE `cover_art` `cover_art` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'assets/images/survey.jpg';
-ALTER TABLE `surveys_questions` ADD `instructions` VARCHAR(1000) NULL DEFAULT NULL AFTER `title`;
+INSERT INTO `surveys_questions` VALUES (
+NULL, 1, '1', 'Promising Influencer OTY', '', 'multiple',
+'[\"@Ruf_ayi\",\"@nanaKwameFlex\",\"@fawogyimiiko\",\"@Sark_Lawyer\",\"@akosuaampofowah\",\"@Afia_Dimple\",\"@AddyMavis\",\"@dr_doreeeen\",\"@Berneese_\",\"@ama_serwaaa\",\"@naturewaaa\",\"@akuahwritess\",\"@ama_adoma\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Diaspora Tweep OTY', '', 'multiple',
+'[\"@_papayawgh\",\"@nickirich14\",\"@worldofowusu\",\"@ohemaakaly\",\"@toriious\",\"@dj_backyard\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Football Banter OTY', '', 'multiple',
+'[\"@thatEsselGuy\",\"@Opresii\",\"@MmoaNkoaaa\",\"@the_marcoli_boy\",\"@KayPoissonOne\",\"@AsabereRoland\",\"@ilatif\",\"@founda\",\"@KeleweleJoint\",\"@_gyesi\",\"@the_presider\",\"@_sharyf\",\"@ellyserwaaa\",\"@Teemah433\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Best Punter OTY', '', 'multiple',
+'[\"@fawogyimiiko\",\"@Bonty_Official_\",\"@Opresii\",\"@LilMoGh\",\"@GhanamanTips\",\"@SateSliptips\",\"@enokay69\",\"@iam_presider\",\"@GioPredictor\",\"@GameAnalyst3\",\"@ama_etwepa1\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Sports Influencer OTY', '', 'multiple',
+'[\"Owuraku Ampofo\",\"Benaiah\",\"Fancy Dimaria\",\"Saddick Adams\",\"Kwame Dela\",\"Juliet Bawuah\",\"Yaw Ampofo\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Twitter Space OTY', '', 'multiple',
+'[\"Drive Sneaker Nyame\",\"Vawulence Space\",\"Sports Corner\",\"Find A Match\",\"Kalyjay Space\",\"DNBP\",\"SpacesWithEdwardAsare\"]', 
+'0',now(),'1','1');
+
+INSERT INTO `surveys_questions` VALUES (
+NULL, 1, '1', 'SME (Small and Medium Enterprise) OTY', '', 'multiple',
+'[\"@ForLinkIn\",\"@Shaibu_AB\",\"@thetoosweetguy\",\"@Efo_Edem\",\"@LegonMedikal\",\"@MohPlayInc_\",\"@KwabsGroups\",\"@HazardJerseyHub\",\"@yaa_purple954\",\"@sister_Grr\",\"@ForeverPurple17\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'SME (Small and Medium Enterprise) OTY', '', 'multiple',
+'[\"@ForLinkIn\",\"@Shaibu_AB\",\"@thetoosweetguy\",\"@Efo_Edem\",\"@LegonMedikal\",\"@MohPlayInc_\",\"@KwabsGroups\",\"@HazardJerseyHub\",\"@yaa_purple954\",\"@sister_Grr\",\"@ForeverPurple17\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Most Engaging Tweep OTY', '', 'multiple',
+'[\"@Kojo_Bankz99\",\"@Kayjnr10\",\"@adofoasa_\",\"@IzzatElKhawaja\",\"@lawslaw\",\"@Kwame_Oliver\",\"@MmoaNkoaaa\",\"@unrulyking00\",\"@Iamabena\",\"@Ghana_Yesu\",\"@amaadoma\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Most Engaging Celebrity OTY', '', 'multiple',
+'[\"@CAPTAINPLANETGH\",\"@Anita_Akuffo\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Political Influencer OTY', '', 'multiple',
+'[\"@AnnanPerry\",\"@quame_age\",\"@CheEsquire\",\"@GhanaSocialU\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Photographer OTY', '', 'multiple',
+'[\"@__theseyram\",\"@iamwizgh\",\"@Kormla4\",\"@olando_shots\",\"@tysonphoto\",\"@IkeDeModel1\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Artiste Fanbase OTY', '', 'multiple',
+'[\"Sarknation\",\"Bhimnation\",\"Shatta Movement\",\"Blacko Tribe\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Funniest Tweep OTY', '', 'multiple',
+'[\"@KayPoissonOne\",\"@Opresii\",\"@SneakerNyame_\",\"@Jason_gh1\",\"@koboateng\",\"@thatEsselguy\",\"@daddys_girltn\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Influencer OTY', '', 'multiple',
+'[\"@AsieduMends\", \"@SneakerNyame_\", \"@BenopaOnyx1\", \"@Gyaigyimii\", \"@Opresii\", \"@thatEsselguy\", \"@Kayjnr10\", \"@_liptonia\", \"@daddys_girltn\", \"@Efua_ampofoa\", \"@Aboa_Banku1\", \"@the_marcoli_boy\", \"@KayPoissonOne\", \"@Jason_gh1\", \"@ellyserwaaa\", \"@I_Am_Winter\", \"@Ghana_Yesu\", \"@ilatif\", \"@_Sharyf\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Music Promoter OTY', '', 'multiple',
+'[\"@Donsarkcess\",\"@okt_ranking30\",\"@Aboa_Banku1\",\"@the_marcoli_boy\",\"@Ghana_Yesu_\",\"@MmoaNkoaaa\",\"@SneakerNyame_\",\"@unrulyking00\",\"@ama_adoma\"]', 
+'0',now(),'1','1');
+
+INSERT INTO `surveys_questions` VALUES (
+NULL, 1, '1', 'Trend/Promo Team OTY', '', 'multiple',
+'[\"A1 Influencers\",\"Ultimate Trends\",\"SPL\",\"Gigs Fie\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Content Creator OTY:', '', 'multiple',
+'[\"Aboa Banku & Papsy\",\"@davidentertaina\",\"@sdkdele\",\"@thepowderguy1\",\"@ComedianWaris\",\"@madeinghana\",\"@fixondennis\",\"@HeadlessYoutube\",\"@kwadwosheldon\",\"@EvianaGh\",\"@Wode_Maya\",\"@SportsCorner\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Health Influencer OTY:', '', 'multiple',
+'[\"@GeorgeAnagli\",\"@medSaaka\",\"@dr_doreeeen\",\"@officialSilasMD\",\"@kwesi_bimpe\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Tech Brand OTY', '', 'multiple',
+'[\"MohPlay Inc\",\"For LinkIn\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Cryptocurrency Brand OTY', '', 'multiple',
+'[\"@Apkjnr\",\"@KojoForex\",\"@manofserendipty\",\"@efo_phil\",\"@huclark_\",\"@_owula\",\"@leslie_kkay\",\"@joesacky\",\"@B_Bestbrain\",\"@Ghcryptoguy\"]', 
+'0',now(),'1','1'), (
+NULL, 1, '1', 'Radio/TV Show OTY', '', 'multiple',
+'[\"Date Rush on @tv3_ghana\",\"United Showbiz on @utvghana\",\"Showbiz 360 on @tv3_ghana\",\"National Science & Maths Quiz\",\"Citi CBS on @citi973\",\"Pure Drive on @pure957fm\",\"JOY SMS on @joy997fmDay\",\"Break Hitz – Hitz FM (Andy Dosty)\",\"Entertainment Review – Peace FM (Kwesi Aboagye)\",\"Showbiz A to Z – Joy FM (George Quaye)\",\"For the culture – ABN Radio One (Porfolio and DJ Slim)\",\"Entertainment GH – Neat FM (Ola Michael)\"]', 
+'0',now(),'1','1');
